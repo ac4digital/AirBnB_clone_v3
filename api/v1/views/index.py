a3e00db38923bbe3e1e05f8bace147e062341291
Andrés Cataño <3866@holbertonschool.com>
2022-05-28 20:55:30 -0500

    Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,27 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """return a JSON file with message status OK"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """return a count of classes"""
+    for k, v in classes.items():
+        classes[k] = storage.count(v)
+    return jsonify(classes)
