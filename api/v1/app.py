1cb411d7137f2d4e5b1a6336f5571b6de34285ed
sebasrengi <sebas.rengi@hotmail.com>
2022-05-31 23:00:23 -0500

    point6-8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-"""app to connect to the API"""
-
-import os
-from api.v1.views import app_views
+"""main app file for Flask instance in REST API
+"""
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
 from models import storage
-from flask import Flask, Blueprint, jsonify
-
+from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
-app.register_blueprint(app.views)
+
+def page_not_found(e):
+    """404 error json response"""
+    return jsonify({'error': "Not found"}), 404
 
 
 @app.teardown_appcontext
-def teardowm_appcontext(param):
-    """teardown_appcontext"""
+def teardown_appcontext(exc=None):
+    """called on teardown of app contexts of flask
+    """
     storage.close()
 
 
-@app.page_not_found(404)
-def page_not_found(error):
-    """error response 404"""
-    return jsonify({"error": "Not found"}, 404)
-
-
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    """run the app if the script is not being imported
+    """
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+    app.register_error_handler(404, page_not_found)
+    fetched_host = os.environ.get('HBNB_API_HOST')
+    fetched_port = os.environ.get('HBNB_API_PORT')
+    if fetched_host is None:
+        fetched_host = '0.0.0.0'
+    if fetched_port is None:
+        fetched_port = 5000
+    app.run(host=fetched_host, port=fetched_port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""index views"""
-
+"""index file, main view file
+"""
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
 from models import storage
 
-classes = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+
+@app_views.route('/status', strict_slashes=False)
+def service_status():
+    """returns the status of the RESTful service"""
+    """ TODO check if this formatting is okay for json response with
+        holberton checker """
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """return a JSON file with message status OK"""
-    return jsonify({"status": "OK"})
+@app_views.route('/stats', strict_slashes=False)
+def object_stats():
+    """returns the count of object types"""
 
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """return a count of classes"""
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+        }
     for k, v in classes.items():
         classes[k] = storage.count(v)
     return jsonify(classes)
