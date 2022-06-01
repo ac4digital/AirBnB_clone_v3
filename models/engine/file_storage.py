51a7ef80d63b069d45c452ec1b4ce6f6b6335fcd
sebasrengi <sebas.rengi@hotmail.com>
2022-05-31 22:42:23 -0500

    comit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,14 +64,6 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
-  
-  def count(self, cls=None):
-        """counts the amount of objects matching cls"""
-        if cls is None:
-            return len(self.all())
-        if type(cls) != str:
-            return 0
-        return len(self.all(cls))
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
